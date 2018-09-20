fcebd19ecb583dd76112c0c4d2c1d9b1c2f0d766
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-19 21:11:47 -0500

    wip

--- src/FilterLists.Services/FilterList/FilterListService.cs
+++ src/FilterLists.Services/FilterList/FilterListService.cs
@@ -29,14 +29,10 @@ namespace FilterLists.Services.FilterList
                                Id = (int)l.Id,
                                LanguageIds = l.FilterListLanguages.Select(ll => (int)ll.LanguageId).ToList(),
                                Name = l.Name,
-                               RuleCount = l.Id == 1000 //l.Snapshots.Any(s => s.WasSuccessful)
-                                   ? l.Snapshots
-                                      .Where(s => s.WasSuccessful)
-                                      .OrderByDescending(s => s.CreatedDateUtc)
-                                      .FirstOrDefault()
-                                      .SnapshotRules
-                                      .Count
-                                   : (int?)null,
+                               RuleCount = DbContext.Snapshots
+                                                    .Where(s => s.WasSuccessful && s.FilterList == l)
+                                                    .Select(s => (int?)s.SnapshotRules.Count)
+                                                    .FirstOrDefault(),
                                SyntaxId = (int)l.SyntaxId,
                                TagIds = l.FilterListTags.Select(lt => (int)lt.TagId).ToList(),
                                UpdatedDate = l.Snapshots.Count(s => s.WasSuccessful && s.WasUpdated) >= 2
