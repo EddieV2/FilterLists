ec7d84ee236c1dbb0ccd20f013935deee2155a7e
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-22 16:46:51 -0500

    wip

--- src/FilterLists.Web/ClientApp/modules/home/components/listsTable/ListsTable.tsx
+++ src/FilterLists.Web/ClientApp/modules/home/components/listsTable/ListsTable.tsx
@@ -27,48 +27,8 @@ export const ListsTable = (props: IProps) => {
                defaultPageSize={props.pageSize}
                showPageSizeOptions={false}
                columns={[
-                   {
-                       Header: "Name",
-                       accessor: "name",
-                       filterable: true,
-                       filterMethod: (f: any, r: any) =>
-                           r[f.id].toUpperCase().includes(f.value.toUpperCase()),
-                       sortMethod: (a: any, b: any) => a.toUpperCase() > b.toUpperCase() ? 1 : -1,
-                       Cell: (c: any) => <h2 className="mb-0">{c.value}</h2>
-                   },
-                   {
-                       Header: "Software",
-                       accessor: "syntaxId",
-                       filterable: true,
-                       filterMethod: (f: any, r: any) =>
-                           f.value === "any" ||
-                           (r[f.id]
-                               ? props.software.filter(s => s.id.includes(r[f.id])).includes(f.value)
-                                : false),
-                       Filter: ({ filter, onChange }) =>
-                           <select
-                               onChange={(event: any) => onChange(event.target.value)}
-                               style={{ width: "100%" }}
-                               value={filter ? filter.value : "any"}>
-                               <option value="any">Any</option>
-                               {props.software
-                                    ? props.software.map((s: ISoftwareDto, i: number) =>
-                                        <option value={s.id} key={i}>{s.name}</option>)
-                                    : null
-                               }
-                           </select>,
-                       sortable: false,
-                       Cell: (c: any) => c.value
-                                         ? props.software.filter(s => s.syntaxIds.indexOf(c.value) !== 0).map(
-                                             (s: ISoftwareDto, i: number) =>
-                                             <SoftwareIcon id={s.id} key={i}/>)
-                                         : null,
-                       width: 155,
-                       headerClassName: "d-none d-md-block",
-                       className: "d-none d-md-block",
-                       show: props.columnVisibility.filter(
-                           (c: IColumnVisibility) => { return c.column === "Software"; })[0].visible
-                   },
+                   Name,
+                   Software(props),
                    //{
                    //    Header: "Tags",
                    //    accessor: "tags",
@@ -181,4 +141,47 @@ export const ListsTable = (props: IProps) => {
                ]}
                SubComponent={(r: any) => <DetailsExpander listId={r.original.id}/>}
                className="-striped -highlight"/>;
+};
+
+const Name = {
+    Header: "Name",
+    accessor: "name",
+    filterable: true,
+    filterMethod: (f: any, r: any) => r[f.id].toUpperCase().includes(f.value.toUpperCase()),
+    sortMethod: (a: any, b: any) => a.toUpperCase() > b.toUpperCase() ? 1 : -1,
+    Cell: (c: any) => <h2 className="mb-0">{c.value}</h2>
+};
+
+const Software = (props: any) => {
+    return {
+        Header: "Software",
+        accessor: "syntaxId",
+        filterable: true,
+        filterMethod: (f: any, r: any) =>
+            f.value === "any" ||
+            (r[f.id]
+                 ? props.software.filter(s => s.id.includes(r[f.id])).includes(f.value)
+                 : false),
+        Filter: ({ filter, onChange }: any) =>
+            <select
+                onChange={(event: any) => onChange(event.target.value)}
+                style={{ width: "100%" }}
+                value={filter ? filter.value : "any"}>
+                <option value="any">Any</option>
+                {props.software
+                     ? props.software.map(
+                         (s: ISoftwareDto, i: number) => <option value={s.id} key={i}>{s.name}</option>)
+                     : null}
+            </select>,
+        sortable: false,
+        Cell: (c: any) => c.value
+                          ? props.software.filter((s: any) => s.syntaxIds.indexOf(c.value) !== 0).map(
+                              (s: ISoftwareDto, i: number) =>
+                              <SoftwareIcon id={s.id} key={i}/>)
+                          : null,
+        width: 155,
+        headerClassName: "d-none d-md-block",
+        className: "d-none d-md-block",
+        show: props.columnVisibility.filter((c: IColumnVisibility) => { return c.column === "Software"; })[0].visible
+    };
 };
